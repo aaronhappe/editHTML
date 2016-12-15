9720247fcb24b962872e43c7dc4c5126f112e144
Aaron Happe <ahappe@brownshoe.com>
2016-12-15 12:56:48 -0600

    cleaning up

--- editHTML.js
+++ editHTML.js
@@ -1,53 +1,86 @@
+// create elements
 var myEditor = $("<div/>",{
     class : "myEditor"
 }),
 ulShops = $("<ul/>",{
     class : "shopName"
 }),
+
+// regular expressions
 shopStartRegGlobal = new RegExp(/<\!-- Shop:\d+_([\w-_/d]+\s\[[\w]+\])\sSTART -->/, "g"),
-shopStartReg = new RegExp(/<\!-- Shop:\d+_([\w-_/d]+)\s\[[\w]+\]\sSTART -->/),
+shopStartReg = new RegExp(/<\!-- (Shop:\d+_[\w-_/d]+\s\[[\w]+\]\sSTART) -->/),
 shopEndRegGlobal = new RegExp(/<\!-- Shop:\d+_([\w-]+\s\[[\w]+\])\sEND -->/, "g"),
+
+//perform operation to get value
 bodyHTML = $('body').html(),
 shopMatchs = bodyHTML.match(shopStartRegGlobal),
+
+//declare vars
 shopLi = "",
-$shopLi = $("ul.shopName li"),
-$selectedShop = "";
+$selectedShop = "",
+shopNameMatchArr = [],
+funcs = {};
 
-shopLi = $.map(shopMatchs, function( val ){
-     var shopNameMatch = val.match(shopStartReg),
-     shopNames = $("<li/>", {
-        text: shopNameMatch[1]
-     }) ;
+funcs.init = function(){
+
+    funcs.findFullShopName();
+
+    funcs.createUI();
+
+    funcs.clickHandlers();
+}
+
+funcs.createUI = function(){
+    ulShops.appendTo(myEditor);
+    myEditor.appendTo('body');
+
+
+    $("ul.shopName li").css({
+        'cursor' : 'pointer',
+        'paddingTop' : '1em'
+    });
+}
+
+funcs.clickHandlers = function() {
+    $("ul.shopName li").click(function(){
+        $selectedShop = $(this).text();
+        funcs.getAllComments(document.body, $selectedShop);
+    });
+}
+
+funcs.findFullShopName = function(){
+    $.map(shopMatchs, function( val ){
+         var shopNameMatch = val.match(shopStartReg);
+         funcs.createShopLi(shopNameMatch);
+    });
+}
+
+funcs.createShopLi = function (shopNameMatch) {
+     var shopNames = $("<li/>", {
+        text: shopNameMatch[1],
+     });
      shopNames.appendTo(ulShops);
-});
+}
 
-ulShops.appendTo(myEditor);
-myEditor.appendTo('body');
-
-function filterNone() {
+funcs.filterNone = function() {
     return NodeFilter.FILTER_ACCEPT;
 }
 
-function getAllComments(rootElem) {
+funcs.getAllComments = function(rootElem, selectedShop) {
     var comments = [];
     // Fourth argument, which is actually obsolete according to the DOM4 standard, is required in IE 11
-    var iterator = document.createNodeIterator(rootElem, NodeFilter.SHOW_COMMENT, filterNone, false);
+    var iterator = document.createNodeIterator(rootElem, NodeFilter.SHOW_COMMENT, funcs.filterNone, false);
     var curNode;
     while (curNode = iterator.nextNode()) {
 
         var comment = curNode.nodeValue;
         comment = comment.trim();
-        if ("End BlueKai Tag" == comment) {
-            console.log("bingo!");
+        if (comment == selectedShop){
+            console.log(selectedShop);
         }
     }
-    return comments;
 }
 
-$shopLi.click(function(){
-    $selectedShop = $(this).text();
-});
-
 // window.addEventListener("load", function() {
-    getAllComments(document.body);
+    funcs.init();
 // });
